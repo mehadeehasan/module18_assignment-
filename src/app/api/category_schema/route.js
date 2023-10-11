75f0f8215b669c36271d0b5bbc4e40892f3a0144
Md. Mehadee Hasan <69212670+mehadeehasan@users.noreply.github.com>
2023-10-11 15:49:00 +0600

    Add files via upload

---
+++ src/app/api/category_schema/route.js
@@ -0,0 +1,75 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+// Single Insert Data
+
+export async function POST(req, res) {
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+    try {
+      const prisma = new PrismaClient();
+      let reqBody  = await req.json();
+      let result   = await prisma.category.create({ data: reqBody });
+
+      return NextResponse.json({ status: "Success", result: result });
+    } catch (err) {
+      return NextResponse.json({ status: "Fail", result: err.toString() });
+    }
+}
+
+ //Read All Data
+
+export async function GET(req, res) {
+    BigInt.prototype.toJSON = function () {
+      return this.toString();
+    };
+try {
+    const prisma = new PrismaClient();
+    let result = await prisma.category.findMany();
+
+    return NextResponse.json({ status: "Success", result: result });
+  } catch (err) {
+    return NextResponse.json({ status: "Fail", result: err.toString() });
+  }
+}
+
+// Update Single Data
+
+export async function PUT(req, res) {
+    BigInt.prototype.toJSON = function () {
+      return this.toString();
+    };
+try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+    let result = await prisma.category.update({
+      where: reqBody,
+      data: {
+        "metaTitle": "Councilors"
+      },
+    });
+    return NextResponse.json({ status: "Success", result: result });
+  } catch (err) {
+    return NextResponse.json({ status: "Fail", result: err.toString() });
+  }
+}
+
+//Delete  single Data.
+
+export async function DELETE(req, res) {
+    BigInt.prototype.toJSON = function () {
+      return this.toString();
+    };
+try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+    let result = await prisma.category.delete({
+      where: reqBody,
+    });
+    return NextResponse.json({ status: "Success", result: result });
+  } catch (err) {
+    return NextResponse.json({ status: "Fail", result: err.toString() });
+  }
+}
+
---
+++ src/app/api/postCategory_schema/route.js
@@ -0,0 +1,75 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+// Single Insert Data
+
+export async function POST(req, res) {
+  BigInt.prototype.toJSON = function () {
+    return this.toString();
+  };
+    try {
+      const prisma = new PrismaClient();
+      let reqBody  = await req.json();
+      let result   = await prisma.post_category.create({ data: reqBody });
+
+      return NextResponse.json({ status: "Success", result: result });
+    } catch (err) {
+      return NextResponse.json({ status: "Fail", result: err.toString() });
+    }
+}
+
+ //Read All Data
+
+export async function GET(req, res) {
+    BigInt.prototype.toJSON = function () {
+      return this.toString();
+    };
+try {
+    const prisma = new PrismaClient();
+    let result = await prisma.post_category.findMany();
+
+    return NextResponse.json({ status: "Success", result: result });
+  } catch (err) {
+    return NextResponse.json({ status: "Fail", result: err.toString() });
+  }
+}
+
+// Update Single Data
+
+export async function PUT(req, res) {
+    BigInt.prototype.toJSON = function () {
+      return this.toString();
+    };
+try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+    let result = await prisma.post_category.update({
+      where: reqBody,
+      data: {
+        "metaTitle": "Councilors"
+      },
+    });
+    return NextResponse.json({ status: "Success", result: result });
+  } catch (err) {
+    return NextResponse.json({ status: "Fail", result: err.toString() });
+  }
+}
+
+//Delete  single Data.
+
+export async function DELETE(req, res) {
+    BigInt.prototype.toJSON = function () {
+      return this.toString();
+    };
+try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+    let result = await prisma.post_category.delete({
+      where: reqBody,
+    });
+    return NextResponse.json({ status: "Success", result: result });
+  } catch (err) {
+    return NextResponse.json({ status: "Fail", result: err.toString() });
+  }
+}
+
